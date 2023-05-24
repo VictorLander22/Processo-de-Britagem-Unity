5fd98c69d7e8436b1b3556b2efc4ca74f33f35ce
Victor Daniel Lander <victordlander@hotmail.com>
2023-05-24 15:04:30 -0300

    inicio esteira

--- Assets/Scripts/Esteira.cs
+++ Assets/Scripts/Esteira.cs
@@ -6,6 +6,16 @@ public class Esteira : MonoBehaviour
 {
     private GameObject esteira;
 
+    private bool state = false;
+
+    Transform filho;
+
+    [SerializeField]
+    private List<Rigidbody> britasColididas = new List<Rigidbody>(); // Lista de britas que entraram en contato com a esteira.
+
+    [SerializeField]
+    private float forcaEsteira = 1.0f;
+
     [SerializeField]
     private AudioManager aud;
 
@@ -20,21 +30,28 @@ public class Esteira : MonoBehaviour
         {
             Debug.LogError("Aud null");
         }
+
+        addRoletaColider();
     }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        AplicarForcaBrita();
+    }
 
     public void ligar()
     {
         Debug.LogWarning("Esteira Ligada");
         startAnimations();
+        state = true;
         aud.Play("Esteira");
     }
 
     public void parar()
     {
         stopAnimations();
+        state = false;
         aud.Stop("Esteira");
     }
 
@@ -60,6 +77,64 @@ public class Esteira : MonoBehaviour
             if (anim.enabled)
             {
                 anim.enabled = false;
+            }
+        }
+    }
+
+    private void addRoletaColider()
+    {
+        filho = transform.Find("Roleta");
+
+        if (filho != null)
+        {
+            // Obtém o MeshCollider do objeto filho encontrado
+            MeshCollider meshCollider = filho.GetComponent<MeshCollider>();
+
+            if (meshCollider != null)
+            {
+                // Faça algo com o MeshCollider
+                Debug.Log("MeshCollider encontrado no objeto filho ");
+            }
+            else
+            {
+                Debug.Log("Nenhum MeshCollider encontrado no objeto filho ");
+            }
+        }
+        else
+        {
+            Debug.Log("Nenhum objeto filho com o nome  encontrado.");
+        }
+    }
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     // Verifica se o objeto colidiu com o objeto da esteira
+    //     if (collision.gameObject.CompareTag("Brita2"))
+    //     {
+    //         Debug.LogWarning("Brita2 colidiu com o hit box");
+
+    //         // Obtém o componente Rigidbody do objeto colidido
+    //         Rigidbody rigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+    //         // Verifica se o objeto possui um Rigidbody
+    //         if (rigidbody != null)
+    //         {
+    //             Debug.LogWarning("1");
+    //             // Adiciona o Rigidbody à lista de objetos colididos
+    //             britasColididas.Add(rigidbody);
+    //         }
+    //     }
+    // }
+
+    // Mover brita
+    public void AplicarForcaBrita()
+    {
+        foreach (Rigidbody brita in britasColididas)
+        {
+            if (brita != null)
+            {
+                // Aplica a força no objeto ao longo do eixo X positivo
+                brita.AddForce(Vector3.left * forcaEsteira);
             }
         }
     }
